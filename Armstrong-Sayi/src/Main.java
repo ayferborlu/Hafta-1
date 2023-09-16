bf0b0fc3f1e3767b10c6574d8c2f44aa4b9fbf6a
ayferborlu <142981819+ayferborlu@users.noreply.github.com>
2023-09-17 00:23:41 +0300

    ATM

--- Armstrong-Sayi/src/Main.java
+++ Armstrong-Sayi/src/Main.java
@@ -1,17 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı girin: ");
+        int number = input.nextInt();
+        int basNumber = 0;
+        int tempnumber = 0;
+        int basvalue;
+        int toplam=0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while (tempnumber != 0) {
+        tempnumber /= 10;
+        basNumber++;
+        toplam+=number/10;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
-}
+    tempnumber = number;
+        while( tempnumber != 0){
+         basvalue=   tempnumber % 10;
+            System.out.println(basvalue);
+            tempnumber/= 10;
+            toplam+=number/10;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+}}
