5e9a6caa38d6cd9726e349234f7ad0eff23f72a7
ayferborlu <142981819+ayferborlu@users.noreply.github.com>
2023-09-17 13:14:46 +0300

    Ters Üçgen Yapımı

--- Armstrong-Sayi/src/Main.java
+++ Armstrong-Sayi/src/Main.java
@@ -5,34 +5,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Sayı girin: ");
         int number = input.nextInt();
-        int basNumber = 0;
         int tempnumber = 0;
         int basvalue;
-        int toplam=0;
+        int toplam = 0;
 
+        tempnumber = number;
         while (tempnumber != 0) {
-        tempnumber /= 10;
-        basNumber++;
-        toplam+=number/10;
-
-    }
-    tempnumber = number;
-        while( tempnumber != 0){
-         basvalue=   tempnumber % 10;
-            System.out.println(basvalue);
-            tempnumber/= 10;
-            toplam+=number/10;
-
-
+            basvalue = tempnumber % 10;
+            tempnumber /= 10;
+            toplam += basvalue;
         }
-
-
-
-
-
-
-
-
-
-
-}}
+        System.out.println(toplam);
+    }
+}
--- TersUcgenYapimi/src/Main.java
+++ TersUcgenYapimi/src/Main.java
@@ -1,12 +1,24 @@
-import java. util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Kaç satır olsun: ");
-        int n =input.nextInt();
+        int n = input.nextInt();
 
-
+        var yildizSayisi = n;
+        for (int i = 0; i < n; i++) {
+            // baştaki boşlukları oluşturan for:
+            for (int m = 0; m < i; m++) {
+                System.out.print(" ");
+            }
+            // yıldızları basan for:
+            for (int j = 0; j <= yildizSayisi; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            yildizSayisi -= 2;
+        }
     }
 }
